098b64faa5acdcc1f03a675a373a5dcd3ab8933b
Joseph Pickard <joseph.pickard77@gmail.com>
2019-05-16 16:52:32 -0400

    Add files via upload
    Updated Map to Focus on Geological Center of United States with a modified zoom level

--- map generation.js
+++ map generation.js
@@ -1,5 +1,5 @@
 document.getElementById("h1").innerHTML = "NCL Map";
 function initMap() {
-    var kokomo = {lat: 40.4864, lng: -86.1336};
-    var map = new google.maps.Map(d3.select("#map").node(),{zoom:15, center:kokomo});
+    var center = {lat: 39.83333, lng: -98.58333};
+    var map = new google.maps.Map(d3.select("#map").node(),{zoom:3, center:center});
 }
